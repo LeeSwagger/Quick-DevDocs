af6e3ced309639b83ce23f7a8d64641693897264
Arianrhod <theguidanceofawhitetower@gmail.com>
2015-10-21 23:38:27 +0800

    make codes in gulpfile more expressive

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,45 +7,42 @@ import nib from 'nib'
 import del from 'del'
 
 gulp
-  .task('jade', () => {
-    gulp.src('./app/pages/*.jade')
-      .pipe(jade())
-      .pipe(gulp.dest('./app/pages/build'))
-  })
-  .task('stylus', () => {
-    gulp.src('./app/styles/*.styl')
-      .pipe(stylus({
-        use: nib(),
-        compress: true,
-        import: 'nib'
-      }))
-      .pipe(gulp.dest('./app/styles/build'))
-  })
-  .task('babel', function() {
-    gulp.src('./app/scripts/*.js?(x)')
-      .pipe(babel())
-      .pipe(gulp.dest('./app/scripts/build'));
-  })
+  .task('jade', () => gulp
+    .src('./app/pages/*.jade')
+    .pipe(jade())
+    .pipe(gulp.dest('./app/pages/build'))
+  )
+  .task('stylus', () => gulp
+    .src('./app/styles/*.styl')
+    .pipe(stylus({
+      use: nib(),
+      compress: true,
+      import: 'nib'
+    }))
+    .pipe(gulp.dest('./app/styles/build'))
+  )
+  .task('babel', () => gulp
+    .src('./app/scripts/*.js?(x)')
+    .pipe(babel())
+    .pipe(gulp.dest('./app/scripts/build'));
+  )
   .task('watch', () => {
     gulp.watch('./app/pages/*.jade', ['jade'])
     gulp.watch('./app/styles/*.styl', ['stylus'])
     gulp.watch('./app/scripts/*.js?(x)', ['babel'])
   })
-  .task('cp', () => {
-    gulp.src([
-        './node_modules/backbone/backbone.min.js',
-        './node_modules/jquery/dist/jquery.min.js',
-        './node_modules/lodash/lodash.min.js'
-      ])
-      .pipe(gulp.dest('./app/components'))
-  })
+  .task('cp', () => gulp
+    .src([
+      './node_modules/backbone/backbone.min.js',
+      './node_modules/jquery/dist/jquery.min.js',
+      './node_modules/lodash/lodash.min.js'
+    ])
+    .pipe(gulp.dest('./app/components'))
+  )
   .task('default', ['cp', 'jade', 'stylus', 'babel', 'watch'])
 
 gulp
-  .task('pack', () => {
-    del('crx', () => {
-      gulp
-        .src(['./app/**/*', '!./app/**/*.styl', '!./app/**/*.jade'])
-        .pipe(gulp.dest('./crx'))
-    })
-  })
+  .task('pack', () => del('crx', () => gulp
+    .src(['./app/**/*', '!./app/**/*.styl', '!./app/**/*.jade'])
+    .pipe(gulp.dest('./crx'))
+  ))
